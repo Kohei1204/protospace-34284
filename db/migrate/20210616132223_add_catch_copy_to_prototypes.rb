65d79c27a0fc4591b0b1ef5072d4be3656b8b87a
Kohei1204 <bigdipper71225@gmail.com>
2021-06-20 15:53:28 +0900

    不要なファイルの削除

--- db/migrate/20210616132223_add_catch_copy_to_prototypes.rb
+++
@@ -1,5 +0,0 @@
-class AddCatchCopyToPrototypes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :prototypes, :catch_copy, :text
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_16_132651) do
+ActiveRecord::Schema.define(version: 2021_06_15_125827) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,21 +36,11 @@ ActiveRecord::Schema.define(version: 2021_06_16_132651) do
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.bigint "prototype_id", null: false
-    t.text "text"
-    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "title"
-    t.text "catch_copy"
-    t.text "concept"
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -70,7 +60,4 @@ ActiveRecord::Schema.define(version: 2021_06_16_132651) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "prototypes"
-  add_foreign_key "comments", "users"
-  add_foreign_key "prototypes", "users"
 end
